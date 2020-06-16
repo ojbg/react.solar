6aa92082ebf73498b58913f13eb101db7180d7d3
iser <contact@iser-labs.com>
2020-06-16 11:42:43 +0300

    Refactor topbar logo and classes

--- frontend/src/components/TopBar/index.js
+++ frontend/src/components/TopBar/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { appTitle } from 'config';
+import { ReactComponent as Logo } from 'assets/Logo.svg';
+import './TopBar.css';
 
 const TopBar = () => {
-  return <div>TopBar</div>;
+  return (
+    <div className='top_bar'>
+      <Logo className='app_logo' />
+      <span className='app_title'>{appTitle}</span>
+    </div>
+  );
 };
 
 export default TopBar;
