13a45f7ab1a704fa757b6f36d3af83f4bdee4a2b
iser <contact@iser-labs.com>
2020-06-16 09:57:27 +0300

    Remove unsued files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import 'App.css';
 import TopBar from 'components/TopBar';
 import Content from 'components/Content';
-import Footer from 'components/Footer';
 
 const App = () => {
   return (
     <div className="App">
       <TopBar />
       <Content />
-      <Footer />
     </div>
   );
 }
