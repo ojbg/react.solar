3a720cc0733adca307af43bc9b0ac9c63421ee20
iser <contact@iser-labs.com>
2020-06-18 01:49:48 +0300

    Add App and Total Power test files

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitForElement} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+    test('render component without errors', async()=> {
+        await waitForElement(()=>render(<App />));
+    });
 });
---
+++ frontend/src/components/TotalPower/TotalPower.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import TotalPower from 'components/TotalPower';
+
+describe('TotalPower component', () => {
+    test('render component without errors', ()=> {
+        render(<TotalPower />);        
+    });
+
+    test('displays formated power value when prop is passed', async() => {
+        const totalPower = 1780;
+        const expected = '1.78 KW';
+        render( <TotalPower totalPower={totalPower} />);
+        expect(await screen.findByText(expected)).toBeInTheDocument();
+    });
+
+    test('displays Not Valid text when invalid prop', async() => {
+        const invalidProp = 'text';
+        const expected = 'Not Valid';
+        render( <TotalPower totalPower={invalidProp} />);
+        expect(await screen.findByText(expected)).toBeInTheDocument();        
+    });
+
+    test('displays default previous power value', async() => {
+        const totalPower = 1500;
+        const expected = 'Previous: 0.00 W';
+        render (<TotalPower totalPower={totalPower}/>);
+        expect(await screen.findByText(expected)).toBeInTheDocument();
+    });
+
+    test('displays previous power value', async() => {
+        const totalPower = 1e6;
+        const totalPowerNext = 2000;
+        const expected = 'Previous: 1.00 MW';
+        const {rerender} = render (<TotalPower totalPower={totalPower}/>);
+        rerender (<TotalPower totalPower={totalPowerNext}/>);
+        expect(await screen.findByText(expected)).toBeInTheDocument();
+    });
+});
