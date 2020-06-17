b7be4810a799315664bc6d4164d891544af1d89d
iser <contact@iser-labs.com>
2020-06-17 15:45:58 +0300

    Refactor total power component

--- frontend/src/components/TotalPower/index.js
+++ frontend/src/components/TotalPower/index.js
@@ -6,8 +6,8 @@ import { Chart } from 'react-google-charts';
 
 const TotalPower = ({ totalPower }) => {
   const title = 'TOTAL POWER';
-
-  const [powerData, setPowerData] = useState([['x', 'Total Power']]);
+  const [updated, setUpdated] = useState('');
+  const [powerData, setPowerData] = useState([['time', 'Total Power (W)']]);
 
   const chartOptions = {
     chartType: 'LineChart',
@@ -17,24 +17,61 @@ const TotalPower = ({ totalPower }) => {
       0: { color: '#f7f7f7' },
     },
     vAxis: {
+      title: 'Watts',
+      titleTextStyle: { color: '#f7f7f7' },
       gridlines: { color: 'transparent' },
       baselineColor: 'transparent',
+      textStyle: { color: '#f7f7f7' },
+      format: 'short',
     },
     hAxis: {
       gridlines: { color: 'transparent' },
       baselineColor: 'transparent',
+      textStyle: { color: '#f7f7f7' },
     },
     loaderMessage: 'Loading...',
   };
 
-  const [updated, setUpdated] = useState('');
+  const chartEvents = [
+    {
+      eventName: 'ready',
+      callback({ chartWrapper }) {
+        if (powerData.length > config.charts.trend.maxPoints) {
+          const data = Array.from(powerData);
+          data.splice(1, 1);
+          setPowerData(data);
+        }
+      },
+    },
+  ];
+
+  function formatPower(power) {
+    return `${(power / 1e3).toFixed(2)} KW`;
+  }
+
+  function getPreviousPower() {
+    if (powerData.length > 2) {
+      const [[, power]] = powerData.slice(-2);
+      return power;
+    }
+    return 0;
+  }
+
+  function getLastPower() {
+    const [[, power]] = powerData.slice(-1);
+    return power;
+  }
+
+  function comparePower() {
+    const previous = getPreviousPower();
+    const last = getLastPower();
+    return last >= previous ? 'trend-green' : 'trend-red';
+  }
 
   useEffect(() => {
-    const date = new Date().toLocaleString('en-US');
-    setUpdated(date);
-
+    const [, date] = new Date().toLocaleString('en-US').split(',');
     const newPower = [date, totalPower];
-
+    setUpdated(date);
     setPowerData((prevPower) => [...prevPower, newPower]);
   }, [totalPower]);
 
@@ -42,7 +79,13 @@ const TotalPower = ({ totalPower }) => {
     <Panel title={title} footer={`Last Update: ${updated}`}>
       <div className='total_power'>
         <div className='total_power_info'>
-          <div className='power'>{`${(totalPower / 1e3).toFixed(2)} KW`}</div>
+          <div className='power'>
+            <span>{formatPower(totalPower)}</span>
+            <span className={comparePower()}></span>
+          </div>
+          <div>
+            <span>Previous: {formatPower(getPreviousPower())}</span>
+          </div>
         </div>
 
         <div className='total_power_chart'>
@@ -51,6 +94,7 @@ const TotalPower = ({ totalPower }) => {
             loader={<div>{chartOptions.loaderMessage}</div>}
             data={powerData}
             options={chartOptions}
+            chartEvents={chartEvents}
           />
         </div>
       </div>
