090e6d5f6a78b27a6ee9162d7fb6eeb7e840be0a
iser <contact@iser-labs.com>
2020-06-18 05:59:13 +0300

    Format code

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, waitForElement} from '@testing-library/react';
+import { render, waitForElement } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
-    test('render component without errors', async()=> {
-        await waitForElement(()=>render(<App />));
-    });
+  test('render component without errors', async () => {
+    await waitForElement(() => render(<App />));
+  });
 });
--- frontend/src/components/TotalPower/TotalPower.test.js
+++ frontend/src/components/TotalPower/TotalPower.test.js
@@ -3,43 +3,43 @@ import { render, screen } from '@testing-library/react';
 import TotalPower from 'components/TotalPower';
 
 describe('TotalPower component', () => {
-    test('render component without errors', ()=> {
-        render(<TotalPower />);        
-    });
+  test('render component without errors', () => {
+    render(<TotalPower />);
+  });
 
-    test('display card header', () => {
-        const expected = 'TOTAL POWER';
-        render (<TotalPower />);
-        expect(screen.getByText(expected)).toBeInTheDocument();
-    });
+  test('display card header', () => {
+    const expected = 'TOTAL POWER';
+    render(<TotalPower />);
+    expect(screen.getByText(expected)).toBeInTheDocument();
+  });
 
-    test('displays formated power value when prop is passed', async() => {
-        const totalPower = 1780;
-        const expected = '1.78 KW';
-        render( <TotalPower totalPower={totalPower} />);
-        expect(await screen.findByText(expected)).toBeInTheDocument();
-    });
+  test('displays formated power value when prop is passed', async () => {
+    const totalPower = 1780;
+    const expected = '1.78 KW';
+    render(<TotalPower totalPower={totalPower} />);
+    expect(await screen.findByText(expected)).toBeInTheDocument();
+  });
 
-    test('displays Not Valid text when invalid prop', async() => {
-        const invalidProp = 'text';
-        const expected = 'Not Valid';
-        render( <TotalPower totalPower={invalidProp} />);
-        expect(await screen.findByText(expected)).toBeInTheDocument();        
-    });
+  test('displays Not Valid text when invalid prop', async () => {
+    const invalidProp = 'text';
+    const expected = 'Not Valid';
+    render(<TotalPower totalPower={invalidProp} />);
+    expect(await screen.findByText(expected)).toBeInTheDocument();
+  });
 
-    test('displays default previous power value', async() => {
-        const totalPower = 1500;
-        const expected = 'Previous: 0.00 W';
-        render (<TotalPower totalPower={totalPower}/>);
-        expect(await screen.findByText(expected)).toBeInTheDocument();
-    });
+  test('displays default previous power value', async () => {
+    const totalPower = 1500;
+    const expected = 'Previous: 0.00 W';
+    render(<TotalPower totalPower={totalPower} />);
+    expect(await screen.findByText(expected)).toBeInTheDocument();
+  });
 
-    test('displays previous power value', async() => {
-        const totalPower = 1e6;
-        const totalPowerNext = 2000;
-        const expected = 'Previous: 1.00 MW';
-        const {rerender} = render (<TotalPower totalPower={totalPower}/>);
-        rerender (<TotalPower totalPower={totalPowerNext}/>);
-        expect(await screen.findByText(expected)).toBeInTheDocument();
-    });
+  test('displays previous power value', async () => {
+    const totalPower = 1e6;
+    const totalPowerNext = 2000;
+    const expected = 'Previous: 1.00 MW';
+    const { rerender } = render(<TotalPower totalPower={totalPower} />);
+    rerender(<TotalPower totalPower={totalPowerNext} />);
+    expect(await screen.findByText(expected)).toBeInTheDocument();
+  });
 });
