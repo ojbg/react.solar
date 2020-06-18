c6335c85d366cdae572ca4159022e37045452314
iser <contact@iser-labs.com>
2020-06-18 03:43:07 +0300

    Add locale default

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,6 +5,7 @@ const config = {
     apiKey: '2a2bca3b98ce4e20b878928aed4ce572',//'df2878e6af684cbd9977a5bc26894f19',  
     apiUrl: 'http://api.planetos.com/v1/datasets/bom_access-g_global_40km/',
     weatherForecastUrl: 'point', 
+    locale: 'en-US',
     maxPanels: 30,
     forecastTimer: 1000*60*5,
     panelsTimer: 10000,
